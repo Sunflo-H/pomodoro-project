ec95dc8e31084f5e1b5d7c72523326f5840fb821
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-04-14 23:08:43 +0900

    setInterval의 호출방식변경 테스트

--- index.js
+++ index.js
@@ -289,43 +289,40 @@ function getLoginState() {
 }
 
 function timer() {
-    return setInterval(function () {
-        // 초가 "00"이면 1초뒤에는 min이 1감소하고 sec는 59가 된다.
-        if (sec === "00") {
-            sec = 59 // 59
-            min--;
-            if (String(min).length === 1) {
-                min = addChar_0(min); //01분...09분을 표현하기 위함
+    // 초가 "00"이면 1초뒤에는 min이 1감소하고 sec는 59가 된다.
+    if (sec === "00") {
+        sec = 59 // 59
+        min--;
+        if (String(min).length === 1) {
+            min = addChar_0(min); //01분...09분을 표현하기 위함
+        }
+    }
+    // 초가 "01초"면 1초뒤에는 "00"초, 이때 "00 : 00 "이면 타이머 종료
+    else if (sec == "01") {
+        sec = "00";
+        if (min == "00") {
+            if (breakTimeState === false) {
+                //포모도로 타이머 종료시 휴식시간으로 변경
+                completePomodoro(localStorage.getItem('currentKey'));
+                changeToBreak();
+                showTimer(optionTime.breakTime);
+                return;
+            }
+            else {
+                //휴식시간 종료시 포모도로로 변경
+                changeToPomodoro();
+                // completeBreak();
             }
         }
-        // 초가 "01초"면 1초뒤에는 "00"초, 이때 "00 : 00 "이면 타이머 종료
-        else if (sec == "01") {
-            sec = "00";
-            if (min == "00") {
-                if (breakTimeState === false) {
-                    //포모도로 타이머 종료시 휴식시간으로 변경
-                    completePomodoro(localStorage.getItem('currentKey'));
-                    changeToBreak();
-                    showTimer(optionTime.breakTime);
-                    return;
-                }
-                else {
-                    //휴식시간 종료시 포모도로로 변경
-                    changeToPomodoro();
-                    // completeBreak();
-                }
-            }
+    }
+    // 위 조건 외에는 초가 --로 정상적으로 흘러간다.
+    else {
+        sec--;
+        if (String(sec).length === 1) {
+            sec = addChar_0(sec); //"01"초... "09"초를 표현
         }
-        // 위 조건 외에는 초가 --로 정상적으로 흘러간다.
-        else {
-            sec--;
-            if (String(sec).length === 1) {
-                sec = addChar_0(sec); //"01"초... "09"초를 표현
-            }
-        }
-        showTimer(min, sec);
-        
-    }, 1000)
+    }
+    showTimer(min, sec);
 }
 
 
@@ -648,7 +645,7 @@ timerStartBtn.addEventListener('click', e => {
     if (localStorage.getItem('currentKey') !== null) {
         if (!run) {
             run = true;
-            timeInterval = timer();
+            timeInterval = setInterval(timer, 1000);
         }
         else {
             run = false;
